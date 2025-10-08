6028f1f54e58f7bae9e6fb93ad487ecc53bba951
romeran14 <romeran14@gmail.com>
2025-10-08 13:43:51 -0400

    set product page on vite config

--- vite.config.js
+++ vite.config.js
@@ -34,6 +34,7 @@ export default defineConfig({
         confirmedrequest: resolve(__dirname, "confirmed-request.html"),
         contact: resolve(__dirname, "contact.html"),
         shop: resolve(__dirname, "shop.html"),
+        product: resolve(__dirname, "product.html"),
         termsConditions: resolve(__dirname, "terms-and-conditions.html"),
         globalPrivacyPolicy: resolve(__dirname, "global-privacy-policy.html"),
         notfound: resolve(__dirname, "not-found.html"),
