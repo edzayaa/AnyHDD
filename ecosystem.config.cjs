31923178de23db2fadc242694f4a0e5158abcf70
calvin <aplasencia661@gmail.com>
2026-01-31 10:10:33 -0500

    feat: Update ecosystem configuration to include interpreter and standardize environment variable structure

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -3,15 +3,18 @@ module.exports = {
         {
             name: "anyhdd",
             script: "build/bin/server.js",
+            interpreter: "/home/master/.nvm/versions/node/v24.13.0/bin/node",
             exec_mode: "cluster",
             instances: "max",
             autorestart: true,
             watch: false,
             max_memory_restart: "900M",
-            env_production: {
+
+            env: {
                 NODE_ENV: "production",
                 PORT: 3333,
             },
+
             error_file: "./.logs/err.log",
             out_file: "./.logs/out.log",
             merge_logs: true,
